09232f7eda46a142fec2a8d08c6920bf8a651079
davidcoats <david@getsafetycone.com>
2021-12-21 16:39:52 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0117.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0117.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for IFileGenerator, IFileOperator, and other file-related extension method base public, common, Rivet-style extensions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0117.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0117.X0001
+{
+    /// <summary>
+    /// IFileGenerator, IFileOperator, and other file-related extension method base public, common, Rivet-style extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
